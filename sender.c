ec8659081d511183017ccf6262869709b3d3579a
korikmat <korikmat@fel.cvut.cz>
2023-04-05 00:43:59 +0200

    Separating into functions and improving appearance

--- sender.c
+++ sender.c
@@ -5,69 +5,87 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #include "packets.h"
 
 #define     BUF_SIZE            1024
-#define     DATA_SIZE           896
 
 #define     TYPE_SYNC           3
 #define     TYPE_ACK            1
 
 #define     ONE_SEC             1
 
-void send_data(char* local_ip_addr, int local_port, char* dest_ip_addr, int dest_port, char* filename) {
-    int socket_desc;
-    struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
+#define     ERROR               -1
 
-    char server_message[BUF_SIZE];
-    char client_message[BUF_SIZE];
-    
-    // Clean buffers:
-    memset(server_message,'\0',sizeof(server_message));
-    memset(client_message,'\0',sizeof(client_message));
-    
+void send_data(char* local_ip_addr, int local_port, char* dest_ip_addr, int dest_port, char* filename);
+void set_ports_and_ip(struct sockaddr_in* client_addr, struct sockaddr_in* server_addr, 
+                char* local_ip_addr, int local_port, char* dest_ip_addr, int dest_port);
+void send_SYNC_packet(int socket_desc, int local_port, char* filename);
+void send_DATA_packet(int socket_desc, char* filename);
+
+void send_data(char* local_ip_addr, int local_port, char* dest_ip_addr, int dest_port, char* filename) {
     // Create socket:
-    socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    
+    int socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if(socket_desc < 0){
         printf("Unable to create socket\n");
-        exit(-1);
+        exit(ERROR);
     }
-    
     printf("Socket created successfully\n");
     
-    // Set MY port and IP:
-    client_addr.sin_family = AF_INET;
-    client_addr.sin_addr.s_addr = inet_addr(local_ip_addr);
-    client_addr.sin_port = htons(local_port);
-
-    // Set RECIVER's port and IP:
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(dest_ip_addr);
-    server_addr.sin_port = htons(dest_port);
+    // Set local and server's port and IP:
+    struct sockaddr_in client_addr;
+    struct sockaddr_in server_addr;
+    set_ports_and_ip(&client_addr, &server_addr, local_ip_addr, local_port, dest_ip_addr, dest_port);
     
     // Bind to the set port and IP:
     if(bind(socket_desc, (struct sockaddr*)&client_addr, sizeof(client_addr)) < 0){
         printf("Couldn't bind to the port\n");
-        exit(-1);
+        exit(ERROR);
     }
     printf("Done with binding\n");
     
     // Send connection request to server:
     if(connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
         printf("Unable to connect\n");
-        exit(-1);
+        exit(ERROR);
     }
     printf("Connected with server successfully\n");
 
-    // Create first packet:
+    // Send first (SYNC) packet and get confirmation from the server:
+    send_SYNC_packet(socket_desc, local_port, filename);
+    
+    // Send the remaining (DATA) packets:
+    send_DATA_packet(socket_desc, filename);
+
+    // Close the socket:
+    close(socket_desc);
+}
+
+void set_ports_and_ip(struct sockaddr_in* client_addr, struct sockaddr_in* server_addr, 
+                char* local_ip_addr, int local_port, char* dest_ip_addr, int dest_port) {
+    // Set MY port and IP:
+    client_addr->sin_family = AF_INET;
+    client_addr->sin_addr.s_addr = inet_addr(local_ip_addr);
+    client_addr->sin_port = htons(local_port);
+
+    // Set RECIVER's port and IP:
+    server_addr->sin_family = AF_INET;
+    server_addr->sin_addr.s_addr = inet_addr(dest_ip_addr);
+    server_addr->sin_port = htons(dest_port);
+}
+
+void send_SYNC_packet(int socket_desc, int local_port, char* filename) {
+
     SYNC_packet_t sync_packet;
+
     sync_packet.type = TYPE_SYNC;
     sync_packet.type <<= 6;
     sync_packet.sender_port = local_port;
     strCpy(sync_packet.filename, filename);
+
+    char server_message[BUF_SIZE];
+    char client_message[BUF_SIZE];
 
     memcpy(client_message, (char*)&sync_packet, BUF_SIZE);
 
@@ -75,56 +93,65 @@ void send_data(char* local_ip_addr, int local_port, char* dest_ip_addr, int dest
     ack_packet.hash = 0;
     ack_packet.state = 0;
 
-    while (ack_packet.hash>>30 != TYPE_ACK || ack_packet.state != 1) {
+    while (true) {
+        // Send messege to server:
         if(send(socket_desc, client_message, strlen(client_message), 0) < 0){
-            printf("Unable to send message\n");
-            exit(-1);
+            printf("Unable to send message, trying again!\n");
+            sleep(ONE_SEC);
+            continue;
         }
 
         // Receive the server's response:
         if(recv(socket_desc, server_message, sizeof(server_message), 0) < 0){
             printf("Error while receiving server's msg\n");
-            exit(-1);
+            exit(ERROR);
         }
 
+        // Copy to char array data from server:
         memcpy(&ack_packet, (ACK_packet_t*)server_message, BUF_SIZE);
-
+        
+        // Check recieved data:
         if (ack_packet.hash>>30 != TYPE_ACK || ack_packet.state != 1) {
+            printf("Something went wrong. Trying to send the first package again\n");
             sleep(ONE_SEC);
+        } else {
+            printf("The first package was received successfully!\n");
+            break;
         }
     }
-    
+}
+
+void send_DATA_packet(int socket_desc, char* filename) {
+    // Open file stream:
     FILE* file = fopen(filename, "rb");
     if (file == NULL) {
         printf("Error opening file");
-        exit(-1);
+        exit(ERROR);
     }
 
-    // Send the message to server:
+    // Send the DATA to server:
     DATA_packet_t data_packet;
+    
+    char client_message[BUF_SIZE];
+
+    // Read file's data and send to server:
     data_packet.packet_n = 0;
     while (!feof(file)) {
-        fread(data_packet.data, sizeof(char), DATA_SIZE, file);
-        data_packet.hash = 0;
-        data_packet.CRC = 0;
-        data_packet.CRC_remainder = 0;
+        fread(data_packet.data, sizeof(char), sizeof(data_packet.data), file);
+        data_packet.hash = 0;   // Hash 
+        data_packet.CRC = 0;          // and CRC                   
+        data_packet.CRC_remainder = 0;         // recuired!!!
         data_packet.packet_n++;
 
         memcpy(client_message, (char*)&data_packet, BUF_SIZE);
         if(send(socket_desc, client_message, strlen(client_message), 0) < 0){
             printf("Unable to send message\n");
-            exit(-1);
+            exit(ERROR);
         }
         
         sleep(ONE_SEC);
     }
+    printf("All data was successfully sent!\n");
+    // Close file stream:
     fclose(file);
-    
-    // Close the socket:
-    close(socket_desc);
-    
-
-
-
-    return 0;
 }
